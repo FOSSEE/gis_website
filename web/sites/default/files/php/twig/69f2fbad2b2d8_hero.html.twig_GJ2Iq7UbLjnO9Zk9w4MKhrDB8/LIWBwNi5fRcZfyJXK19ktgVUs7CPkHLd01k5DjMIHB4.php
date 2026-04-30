9c93b47c57abbb9b6febd4988dd2b4cb820b0442
akshay9085 <akshaythakur9085@gmail.com>
2026-04-30 12:26:32 +0530

    new theme css and ckeditor plugin pack added;

---
+++ web/sites/default/files/php/twig/69f2fbad2b2d8_hero.html.twig_GJ2Iq7UbLjnO9Zk9w4MKhrDB8/LIWBwNi5fRcZfyJXK19ktgVUs7CPkHLd01k5DjMIHB4.php
@@ -0,0 +1,129 @@
+<?php
+
+use Twig\Environment;
+use Twig\Error\LoaderError;
+use Twig\Error\RuntimeError;
+use Twig\Extension\CoreExtension;
+use Twig\Extension\SandboxExtension;
+use Twig\Markup;
+use Twig\Sandbox\SecurityError;
+use Twig\Sandbox\SecurityNotAllowedTagError;
+use Twig\Sandbox\SecurityNotAllowedFilterError;
+use Twig\Sandbox\SecurityNotAllowedFunctionError;
+use Twig\Source;
+use Twig\Template;
+use Twig\TemplateWrapper;
+
+/* @nava/parts/header/hero.html.twig */
+class __TwigTemplate_641eecbbbc535a0e37c9d0e9fac1ca4e extends Template
+{
+    private Source $source;
+    /**
+     * @var array<string, Template>
+     */
+    private array $macros = [];
+
+    public function __construct(Environment $env)
+    {
+        parent::__construct($env);
+
+        $this->source = $this->getSourceContext();
+
+        $this->parent = false;
+
+        $this->blocks = [
+        ];
+        $this->sandbox = $this->extensions[SandboxExtension::class];
+        $this->checkSecurity();
+    }
+
+    protected function doDisplay(array $context, array $blocks = []): iterable
+    {
+        $macros = $this->macros;
+        // line 1
+        yield $this->extensions['Drupal\Core\Template\TwigExtension']->escapeFilter($this->env, $this->extensions['Drupal\Core\Template\TwigExtension']->attachLibrary("nava/slider"), "html", null, true);
+        yield "
+<section class=\"section-hero full-width\">
+  <div class=\"container\">
+    ";
+        // line 4
+        yield $this->extensions['Drupal\Core\Template\TwigExtension']->escapeFilter($this->env, CoreExtension::getAttribute($this->env, $this->source, ($context["page"] ?? null), "hero", [], "any", false, false, true, 4), "html", null, true);
+        yield "
+  </div>
+</section>
+<script>
+document.addEventListener( 'DOMContentLoaded', function() {
+  var splide = new Splide( '.splide', {
+    type: 'loop',
+    perPage: 1,
+    autoplay: true,
+    rewind: true,
+    pauseOnHover: false,
+    interval: 5000,
+    arrowPath: 'M0.5 1L20 20.5L0.5 41.5',
+  } );
+  splide.mount();
+});
+</script>";
+        $this->env->getExtension('\Drupal\Core\Template\TwigExtension')
+            ->checkDeprecations($context, ["page"]);        yield from [];
+    }
+
+    /**
+     * @codeCoverageIgnore
+     */
+    public function getTemplateName(): string
+    {
+        return "@nava/parts/header/hero.html.twig";
+    }
+
+    /**
+     * @codeCoverageIgnore
+     */
+    public function isTraitable(): bool
+    {
+        return false;
+    }
+
+    /**
+     * @codeCoverageIgnore
+     */
+    public function getDebugInfo(): array
+    {
+        return array (  50 => 4,  44 => 1,);
+    }
+
+    public function getSourceContext(): Source
+    {
+        return new Source("", "@nava/parts/header/hero.html.twig", "/opt/drupal/web/themes/contrib/nava/templates/parts/header/hero.html.twig");
+    }
+    
+    public function checkSecurity()
+    {
+        static $tags = [];
+        static $filters = ["escape" => 1];
+        static $functions = ["attach_library" => 1];
+
+        try {
+            $this->sandbox->checkSecurity(
+                [],
+                ['escape'],
+                ['attach_library'],
+                $this->source
+            );
+        } catch (SecurityError $e) {
+            $e->setSourceContext($this->source);
+
+            if ($e instanceof SecurityNotAllowedTagError && isset($tags[$e->getTagName()])) {
+                $e->setTemplateLine($tags[$e->getTagName()]);
+            } elseif ($e instanceof SecurityNotAllowedFilterError && isset($filters[$e->getFilterName()])) {
+                $e->setTemplateLine($filters[$e->getFilterName()]);
+            } elseif ($e instanceof SecurityNotAllowedFunctionError && isset($functions[$e->getFunctionName()])) {
+                $e->setTemplateLine($functions[$e->getFunctionName()]);
+            }
+
+            throw $e;
+        }
+
+    }
+}
